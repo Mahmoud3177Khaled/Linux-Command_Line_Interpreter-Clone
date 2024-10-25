329204f9258d166f125c9bca8378455514f3cfcc
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2024-10-25 19:06:24 +0300

    My tasks skeleton structure

--- demo/src/main/java/os/cli/Main.java
+++ demo/src/main/java/os/cli/Main.java
@@ -15,12 +15,16 @@ public class Main {
             switch (command) {
                 case "pwd" -> cli.pwd();
                 case "cd" -> cli.cd();
-                case "ls" -> cli.ls();
                 case "mkdir" -> cli.mkdir();
                 case "touch" -> cli.touch();
                 case "mv" -> cli.mv();
                 case "rm" -> cli.rm();
+                case "rmdir" -> cli.rmdir();
                 case "cat" -> cli.cat();
+                case "ls" -> cli.ls();
+                case "uname" -> cli.uname();
+                case "cp" -> cli.cp();
+                case "<" -> cli.inputOp();
                 default -> throw new AssertionError();
             }
         }
@@ -37,35 +41,54 @@ class CLI {
     }
 
     public void pwd() {
-
+        System.out.println("pwd called");
     }
 
     public void cd() {
-
-    }
-
-    public void ls() {
-
+        System.out.println("cd called");
     }
 
     public void mkdir() {
-
+        System.out.println("mkdir called");
     }
 
     public void touch() {
-
+        System.out.println("touch called");
     }
 
     public void mv() {
-
+        System.out.println("mv called");
     }
 
     public void rm() {
+        System.out.println("rm called");
+    }
 
+    // --------------------------- # Mahmoud Khaled 20220317 # --------------------------- //
+
+    public void rmdir() {
+        System.out.println("rmdir called");
     }
 
     public void cat() {
-
+        System.out.println("cat called");
     }
+
+    public void ls() {
+        System.out.println("ls called");
+    }
+
+    public void uname() {
+        System.out.println("uname called");
+    }
+
+    public void cp() {
+        System.out.println("cp called");
+    }
+
+    public void inputOp() {
+        System.out.println("inputOp called");
+    }
+
 
 }
