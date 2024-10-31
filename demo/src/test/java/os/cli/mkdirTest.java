b782799af3a4036d63d2af8d4ab393eac0af3c38
philokaram <146956626+philokaram@users.noreply.github.com>
2024-10-31 15:52:20 +0300

    makeMoreOneDir

--- demo/src/test/java/os/cli/mkdirTest.java
+++ demo/src/test/java/os/cli/mkdirTest.java
@@ -16,4 +16,19 @@ public class mkdirTest {
         assertTrue(f.exists());
         f.delete();
     }
+
+    @Test
+    void makeMoreOneDir(){
+        CLI cli = new CLI("c://");
+        cli.mkdir("test1 test2 test3 test4");
+        File f1 = new File("c://test1");
+        File f2 = new File("c://test2");
+        File f3 = new File("c://test3");
+        File f4 = new File("c://test4");
+        assertTrue(f1.exists() && f2.exists() && f3.exists() && f4.exists());
+        f1.delete();
+        f2.delete();
+        f3.delete();
+        f4.delete();
+    }
 }
