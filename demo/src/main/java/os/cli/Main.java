92d207b63c587a4c25c7ba95ab9502500b8e21d5
philokaram <146956626+philokaram@users.noreply.github.com>
2024-10-31 14:37:01 +0300

    echo 3 tests

--- demo/src/main/java/os/cli/Main.java
+++ demo/src/main/java/os/cli/Main.java
@@ -752,9 +752,7 @@ class CLI {
                     newText += text.charAt(i);
                 }
             }
-            if (newText.length() != 0) {
                 lines.add(newText);
-            }
             if (toFile){
                 File f = new File(currentDir +"\\"+ path);
                 if(!f.exists()){
